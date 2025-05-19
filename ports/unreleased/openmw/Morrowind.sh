80a35dba3ac852b925862a69cc06bbe17eadc6ab
Jeod <47716344+JeodC@users.noreply.github.com>
2025-05-19 12:33:45 -0400

    Move UTRY to released, add OpenMW in progress

---
+++ ports/unreleased/openmw/Morrowind.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+get_controls
+
+$ESUDO chmod 666 /dev/tty1
+$ESUDO chmod 666 /dev/uinput
+
+GAMEDIR=/$directory/ports/openmw
+mkdir -p "$GAMEDIR"
+
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export LD_LIBRARY_PATH="${GAMEDIR}/libs:${LD_LIBRARY_PATH}"
+export XDG_DATA_HOME="$GAMEDIR"
+export XDG_CONFIG_HOME="$GAMEDIR"
+export OPENMW_DECOMPRESS_TEXTURES=1
+#export TEXTINPUTPRESET="Eddie"
+#export TEXTINPUTINTERACTIVE="Y"
+
+$GPTOKEYB2 "openmw" -c "openmw.ini" &
+pm_platform_helper "$GAMEDIR/openmw" > /dev/null
+./morrowind
+
+pm_finish
