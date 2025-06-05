e55f78f1ec272f71991b6da4eb11a5c066e6b092
Jeod <47716344+JeodC@users.noreply.github.com>
2025-06-05 17:57:52 -0400

    Update gms template sh

--- ports/templates/gmloadernext/Portname.sh
+++ ports/templates/gmloadernext/Portname.sh
@@ -24,15 +24,16 @@ cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
-export PATCHER_FILE="$GAMEDIR/tools/patchscript"
-export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
-export PATCHER_TIME="2 to 5 minutes"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export controlfolder
+export DEVICE_ARCH
 
 # Check if we need to patch the game
-if [ ! -f patchlog.txt ] || [ -f $GAMEDIR/assets/data.win ]; then
+if [ ! -f patchlog.txt ] || [ -f "$GAMEDIR/assets/data.win" ]; then
     if [ -f "$controlfolder/utils/patcher.txt" ]; then
+        export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+        export PATCHER_GAME="$(basename "${0%.*}")"
+        export PATCHER_TIME="2 to 5 minutes"
         source "$controlfolder/utils/patcher.txt"
         $ESUDO kill -9 $(pidof gptokeyb)
     else
