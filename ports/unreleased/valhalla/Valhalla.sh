b2487913f48f5963c87787295a16ce40be508cc6
Jeod <47716344+JeodC@users.noreply.github.com>
2025-06-09 13:35:48 -0400

    [VALHALLA] Add valhalla 64bit to unreleased folder
    Only gog version supported for now

---
+++ ports/unreleased/valhalla/Valhalla.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/valhalla"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Check if patchlog.txt to skip patching
+if [ ! -f "$GAMEDIR/patchlog.txt" ]; then
+    if [ -f "$controlfolder/utils/patcher.txt" ]; then
+        export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+        export PATCHER_GAME="$(basename "${0%.*}")"
+        export PATCHER_TIME="2 to 5 minutes"
+        export controlfolder
+        export DEVICE_ARCH
+        source "$controlfolder/utils/patcher.txt"
+        $ESUDO kill -9 $(pidof gptokeyb)
+    else
+        echo "This port requires the latest version of PortMaster."
+    fi
+fi
+
+# Post patcher setup
+export LD_LIBRARY_PATH="$GAMEDIR/lib:$LD_LIBRARY_PATH"
+
+# Display loading splash
+if [ -f "$GAMEDIR/patchlog.txt" ]; then
+    [ "$CFW_NAME" == "muOS" ] && $ESUDO ./tools/splash "splash.png" 1
+    $ESUDO ./tools/splash "splash.png" 2000 &
+fi
+
+# Assign gptokeyb and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "valhalla.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64" > /dev/null
+./gmloadernext.aarch64 -c gmloader.json
+
+# Cleanup
+pm_finish
