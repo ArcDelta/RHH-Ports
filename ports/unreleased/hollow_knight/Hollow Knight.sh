7fbc6874f39262e6d99f7b7e638e890466463ef3
Jeod <47716344+JeodC@users.noreply.github.com>
2025-09-05 07:04:53 -0400

    [HOLLOWKNIGHT] Update port

--- ports/unreleased/hollow_knight/Hollow Knight.sh
+++ ports/unreleased/hollow_knight/Hollow Knight.sh
@@ -29,6 +29,7 @@ cd $GAMEDIR/data
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export LD_LIBRARY_PATH="$GAMEDIR/box64/x64:$GAMEDIR/libs.aarch64:$GAMEDIR/data:$LD_LIBRARY_PATH"
 export BOX64_LD_LIBRARY_PATH="$GAMEDIR/box64/x64:$GAMEDIR/gamedata:$LD_LIBRARY_PATH"
+export XDG_CONFIG_HOME="$GAMEDIR/config" && mkdir -p "$GAMEDIR/config"
 
 # Box64 optimizations
 export BOX64_NOBANNER=1
@@ -42,10 +43,35 @@ export BOX64_VSYNC=0
 export LIBGL_NOERROR=1
 export MESA_NO_ERROR=1
 
+# Display loading splash
+[ "$CFW_NAME" == "muOS" ] && $ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 1 
+$ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 20000 &
+
+swapabxy() {
+    # Update SDL_GAMECONTROLLERCONFIG to swap a/b and x/y button
+
+    if [ "$CFW_NAME" == "knulli" ] && [ -f "$SDL_GAMECONTROLLERCONFIG_FILE" ];then
+	    # Knulli seems to use SDL_GAMECONTROLLERCONFIG_FILE (on rg40xxh at least)
+        cat "$SDL_GAMECONTROLLERCONFIG_FILE" | swapabxy.py > "$GAMEDIR/gamecontrollerdb_swapped.txt"
+	    export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
+    else
+        # Other CFW use SDL_GAMECONTROLLERCONFIG
+        export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | swapabxy.py`"
+    fi
+}
+
+# Swap a/b and x/y button if needed
+if [ -f "$GAMEDIR/swapabxy.txt" ]; then
+    swapabxy
+fi
+
+# Use x11 for game but not for splash
+export SDL_VIDEODRIVER="x11"
+
 # Run it
 $GPTOKEYB $GAME -c "$GAMEDIR/hollowknight.gptk" & 
 pm_platform_helper $GAME > /dev/null
-$BOX64 $GAME
+$BOX64 $GAME -force-opengl -screen-width
 
 #Clean up after ourselves
 pm_finish
