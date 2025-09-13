95bbd8cdb31e10cc881aa9e7862d04dca49fba59
InvisibleFunction <3682184+InvisibleFunction@users.noreply.github.com>
2025-09-13 19:59:45 -0400

    Move debug statement

--- ports/released/hollow_knight/Hollow Knight.sh
+++ ports/released/hollow_knight/Hollow Knight.sh
@@ -47,10 +47,10 @@ else
         export MESA_GL_VERSION_OVERRIDE=3.3
         export MESA_GLSL_VERSION_OVERRIDE=330
         export MESA_NO_ASYNC_COMPILE=1
+
+	# Warn the user in log
+	echo "[WARNING] Overriding GL version to run the game; this may have unintended side effects or performance issues!"
     fi
-    
-    # Warn the user in log
-    echo "[WARNING] Overriding GL version to run the game; this may have unintended side effects or performance issues!"
 fi
 
 # Find game
